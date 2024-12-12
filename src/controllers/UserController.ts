f9dbcd6b580f80a3f43d635d618bfaa85de3db32
Samuel Telles <samuel.santos@outlook.com>
2024-12-12 00:45:27 -0300

    format

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,28 +1,28 @@
-import { Request, Response } from 'express'
-import { UserService } from '../services/UserService'
+import { Request, Response } from "express";
+import { UserService } from "../services/UserService";
 
 export class UserController {
-    userService: UserService
+  userService: UserService;
 
-    constructor(
-        userService = new UserService()
-    ){
-        this.userService = userService
+  constructor(userService = new UserService()) {
+    this.userService = userService;
+  }
+
+  createUser = (request: Request, response: Response): Response => {
+    const user = request.body;
+
+    if (!user.name) {
+      return response
+        .status(400)
+        .json({ message: "Bad request! Name obrigatório" });
     }
 
-    createUser = (request: Request, response: Response): Response => {
-        const user = request.body
+    this.userService.createUser(user.name, user.email);
+    return response.status(201).json({ message: "Usuário criado" });
+  };
 
-        if(!user.name){
-            return response.status(400).json({ message: 'Bad request! Name obrigatório'})
-        }
-
-        this.userService.createUser(user.name, user.email)
-        return response.status(201).json({ message: 'Usuário criado'})
-    }
-
-    getAllUsers = (request: Request, response: Response) => {
-        const users = this.userService.getAllUsers()
-        return response.status(200).json( users )
-    } 
+  getAllUsers = (request: Request, response: Response) => {
+    const users = this.userService.getAllUsers();
+    return response.status(200).json(users);
+  };
 }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import express, { Request, Response } from 'express';
-import { router } from './routes';
+import express, { Request, Response } from "express";
+import { router } from "./routes";
 
 const server = express();
 
-server.use(express.json())
-server.use(router)
+server.use(express.json());
+server.use(router);
 
-server.get('/', (request: Request, response: Response) => {
-    return response.status(200).json({ message: 'DioBank API' })
-})
+server.get("/", (request: Request, response: Response) => {
+  return response.status(200).json({ message: "DioBank API" });
+});
 
-server.listen(5000, () => console.log('Server on'))
+server.listen(5000, () => console.log("Server on"));
